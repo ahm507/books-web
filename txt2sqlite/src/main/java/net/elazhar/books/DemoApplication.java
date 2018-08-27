c9461db272643f434bf392c6c1f6f049453183ee
ahm507 <ahm507@gmail.com>
2018-08-27 11:25:16 +0200

    Refactorings: rename

--- txt2sqlite/src/main/java/net/elazhar/books/DemoApplication.java
+++ txt2sqlite/src/main/java/net/elazhar/books/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package net.elazhar.books;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- txt2sqlite/src/test/java/net/elazhar/books/DemoApplicationTests.java
+++ txt2sqlite/src/test/java/net/elazhar/books/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package net.elazhar.books;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
