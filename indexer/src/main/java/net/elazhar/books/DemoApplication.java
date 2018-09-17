1032008191c387c412ed84d2d7df284c0f67e4b1
ahm507 <ahm507@gmail.com>
2018-09-17 08:55:52 +0200

    Add command line handler and rename class
    The code in this stage does not compile, as class names are changed wuthout renaming the corresponding names.
    This is to preserve file history.

--- indexer/src/main/java/net/elazhar/books/DemoApplication.java
+++ indexer/src/main/java/net/elazhar/books/DemoApplication.java
@@ -2,11 +2,26 @@ package net.elazhar.books;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
-public class DemoApplication {
+public class IndexerApplication implements CommandLineRunner {
+
+
+    private static Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+    @Override 
+    public void run(String... args) throws Exception {
+
+        LOG.info("Application is running");
+
+    }
 }
+
+
--- indexer/src/test/java/net/elazhar/books/DemoApplicationTests.java
+++ indexer/src/test/java/net/elazhar/books/DemoApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class IndexerApplicationTests {
 
 	@Test
 	public void contextLoads() {
