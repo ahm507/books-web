33af3493b7c6162341342f2748eacae86e69ec5c
ahm507 <ahm507@gmail.com>
2018-09-18 09:04:47 +0200

    Store hits

--- txt2fti/src/main/java/net/elazhar/books/indexing/FTIndex.java
+++ txt2fti/src/main/java/net/elazhar/books/indexing/FTIndex.java
@@ -7,7 +7,7 @@ import java.util.TreeSet;
 import java.util.logging.Logger;
 
 public class FTIndex {
-    private HashMap<String, ArrayList<Integer>> index = new HashMap<>();
+    private HashMap<String, ArrayList<SearchHit>> index = new HashMap<>();
     private Logger logger = Logger.getLogger("FTSIndex");
 
     private void showUniqueWords() {
@@ -21,14 +21,16 @@ public class FTIndex {
         logger.info("Total unique words: " + keys.size());
     }
 
-    public void indexWords(String bookId, int pageId, String title, String documentNoVowels) {
+    public void indexWords(String bookId, int docId, String title, String documentNoVowels) {
         String stopWords = "[ \\t,.;:،!?؟\\r\\n(){}\\[\\]«»'\"]-_";
         String[] words = documentNoVowels.split(stopWords);
         for(String word: words) {
             if( ! word.trim().isEmpty()) {
                 //get word
-
-                index.put(word, null);
+                ArrayList<SearchHit> hits = index.get(word);
+                if(hits == null) hits = new ArrayList<>();
+                hits.add(new SearchHit(bookId, docId));
+                index.put(word, hits);
             }
         }
 
--- txt2fti/src/main/java/net/elazhar/books/indexing/SearchHit.java
+++ txt2fti/src/main/java/net/elazhar/books/indexing/SearchHit.java
@@ -2,8 +2,12 @@ package net.elazhar.books.indexing;
 
 public class SearchHit {
 
-    private String bookCode;
-    private int hitId;
+    private String bookId;
+    private int docId;
 
 
+    public SearchHit(String bookId, int docId) {
+        this.bookId = bookId;
+        this.docId = docId;
+    }
 }
