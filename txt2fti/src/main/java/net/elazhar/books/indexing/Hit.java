e96b2949b75f6749779a33c21b10d6e37566f1a6
ahm507 <ahm507@gmail.com>
2018-10-05 10:55:37 +0300

    Implements equals and hashcode

--- txt2fti/src/main/java/net/elazhar/books/indexing/Hit.java
+++ txt2fti/src/main/java/net/elazhar/books/indexing/Hit.java
@@ -1,5 +1,7 @@
 package net.elazhar.books.indexing;
 
+import java.util.Objects;
+
 public class Hit implements Comparable<Hit> {
 
     private String bookId;
@@ -18,8 +20,7 @@ public class Hit implements Comparable<Hit> {
     private String getBookId() {
         return bookId;
     }
-
-
+    
     @Override
     public int compareTo(Hit o) {
         int bookCompare = o.getBookId().compareTo(bookId);
@@ -29,4 +30,30 @@ public class Hit implements Comparable<Hit> {
         return bookCompare;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if(!(obj instanceof Hit)) {
+            return false;
+        }
+        Hit other = (Hit) obj;
+        return other.getBookId().equals(bookId) && other.getDocId() == docId;
+        //You can use Objects.equals()
+    }
+
+    @Override
+    public int hashCode() {
+        //The 17 and 31 hash code idea is from the classic Java book – effective Java : item 9
+//        int result = 17;
+//        result = 31 * result + bookId.hashCode();
+//        result = 31 * result + docId;
+//        return result;
+        return Objects.hash(bookId, docId); //JDK 7
+    }
+
+    @Override public String toString() {
+        return String.format("bookId: %s, docId:", bookId, docId);
+    }
 }
